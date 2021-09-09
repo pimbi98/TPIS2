d5eb2a26c0d0891570716cb874fdda832aab7759
Sebastian <pimbi998@gmail.com>
2021-09-09 19:00:42 -0300

    generamos repositorio raizCuadrada para obtener raiz cuadrada de un N positivo

---
+++ RaizCuadrada.php
@@ -0,0 +1,15 @@
+<!-- Subir un repositorio git a Github con un programa sencillo en PHP (cada estudiante debe inventar una consigna sencilla, un programa que tenga entre 10 y 50 líneas de código aproximadamente).
+
+Dicho repositorio debe tener al menos 3 commits
+
+    Primer commit: La “consigna” (el objetivo) del programa escrita como comentario.
+    Segundo commit: El programa sin concluir, “a mitad de camino”.
+    Commits subsiguientes: Todos los commits intermedios que hagan falta.
+    Último commit: El programa terminado. -->
+<!-- //El programa obtiene a partir de un numero dado, la raiz cuadrada, siempre en caso de ser positivo
+//en caso de ser negativo, otorgara un mensaje de error. -->
+<?php
+
+
+
+
