c38211c2652727bc6bf051bfdf9310db7f5b2ea9
Sebastian <pimbi998@gmail.com>
2021-09-09 19:06:08 -0300

    commit parcial con el html armado

--- RaizCuadrada.php
+++ RaizCuadrada.php
@@ -8,8 +8,30 @@ Dicho repositorio debe tener al menos 3 commits
     Último commit: El programa terminado. -->
 <!-- //El programa obtiene a partir de un numero dado, la raiz cuadrada, siempre en caso de ser positivo
 //en caso de ser negativo, otorgara un mensaje de error. -->
-<?php
-
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "https://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <title>Raìz cuadrada</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?php
+        if ($_SERVER['REQUEST_METHOD']=='POST')
+        {
+            $numero = floatval ($_POST['numero']);
+            if($numero>=0)
+                $raiz_cuadrada=sqrt($numero);
+            else
+            {
+                $raiz_cuadrada=sqrt(-$numero);
+                echo 'ra&iacute;z imaginaria<br>';
+            }
+            echo 'Valor de raiz cuadrada: ' . $raiz_cuadrada . "<br/>\n";
+        }
+        
+        ?>  
+    </body>
+</html>
 
 
 
